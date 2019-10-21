dbef16c35d42b3c3761647003d4612e5ff0a4e7e
Daniel Jones <djones1844@gmail.com>
2019-10-21 12:42:19 -0700

    vue stuff

---
+++ src/components/Finder.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <h1>Quote Finder</h1>
+    <div class="input-group">
+      <input class="form-control" type="text" id="searchInput"> 
+      <div class="input-group-append" >
+        <span class="input-group-text"><img class="search-solid" src="../assets/search-solid.svg"></span>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  props: {}
+}
+</script>
+<style scoped>
+  .search-solid {
+    height: 1.5em;
+    width: 2em;
+  }
+  .input-group {
+    width: 80%;
+    margin-left: 10%;
+  }
+</style>
